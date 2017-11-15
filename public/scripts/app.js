5a1974a468d6686e4e952101aa71faa6271df283
Lilian Shih <l.shih@outlook.com>
2017-11-15 03:50:19 +0000

    can render tweets, from tweets data object, removed hardcoded tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,83 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// const moment = require('moment');
 
+// moment.format();
+
+// Test / driver code (temporary). Eventually will get this from the server.
+var tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+function renderTweets(tweet) {
+  for (tweet in tweetData) {
+    createTweetElement(tweetData[tweet]);
+  }
+}
+
+function createTweetElement(tweet) {
+
+  const $tweet = $("section.tweets-dash").append(`
+    <article class="tweet">
+      <header>
+        <img class="avatar" src="${tweet.user.avatars.small}" alt=""></img>
+        <h2>${tweet.user.name}</h2>
+        <h4>${tweet.user.handle}</h4>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer> ${tweet.created_at}
+        <div class="icons">
+          <i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i>
+        </div>
+      </footer>
+    </article>`);
+  return $tweet;
+};
+
+
+$(document).ready(function() {
+  renderTweets(tweetData);
+});
