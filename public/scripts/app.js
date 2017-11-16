eb859696f3013c04ad4c613a60371e06fa229c56
Lilian Shih <l.shih@outlook.com>
2017-11-16 01:20:57 +0000

    fixed issue with character counter where the counter would not reset to 140 characters after hitting the tweet button, as well as removed html tags from ids and classes (to prevent future issues if tags are changed)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,13 @@
 $(() => {
+
   function main() {
     // the compose box is hidden on page load
-    $("section.new-tweet").hide();
+    $(".new-tweet").hide();
 
     // compose box is toggled by this function
     $("#nav-bar button.compose").on("click", function() {
-      $("section.new-tweet").slideToggle();
-      $("section.new-tweet textarea").select();
+      $(".new-tweet").slideToggle();
+      $(".new-tweet textarea").select();
     })
 
     // this is run whenever page is loaded
@@ -16,17 +17,16 @@ $(() => {
     $("#send-tweet").on("submit", sendNewTweet);
   }
 
-  function daysAgo(numDate) {
-    const dateObj = new Date(numDate);
-    const compare = Date.now() - dateObj;
-    const numOfDays = Math.round(compare / (1000*60*60*24));
-    return numOfDays > 0 ? `${numOfDays} days ago` : `today`;
-  }
-
+  // this ensures that use cannot input script that will mess with other user's experience with Tweeter
+  // currently is vulnerable to attacks via the URL of user's avatar
   function escape(str) {
     const div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
+  }
+
+  function daysAgo(numDate) {
+    return moment(numDate).fromNow();
   }
 
   function renderTweets(tweets) {
@@ -58,17 +58,17 @@ $(() => {
     event.preventDefault();
     const $form = $(this);
     const tweetText = $form.find('textarea[name="text"]').val();
-    if (!tweetText) return alert(`You didn't seem to tweet anything. Please try again.`);
     if (tweetText.length > 140) return alert(`Your tweet is over 140 characters long. Please try again.`);
+    if (!tweetText) return alert(`You didn't seem to tweet anything. Please try again.`);
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: $form.serialize()
     })
     .done(() => {
-      //$(".tweets-dash").prepend(loadTweets(tweetText));
       loadTweets();
-      $("section.new-tweet textarea").val('');
+      $(".new-tweet textarea").val('');
+      $("#counter").text(140);
     })
   }
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
-function countChar() {
-  const maxLength = 140;
-  const compare = maxLength - this.value.length;
-  $("span#counter").text(compare).css("color", compare < 0 ? "red" : "black");
-};
+
 
 $(document).ready(function() {
-  $("textarea#composed-tweet").on("input", countChar);
+  function countChar() {
+    const maxLength = 140;
+    const compare = maxLength - this.value.length;
+    $("#counter").text(compare).css("color", compare < 0 ? "red" : "black");
+  };
+
+  $("#composed-tweet").on("input", countChar);
 });
